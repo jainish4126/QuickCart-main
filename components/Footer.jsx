1899b0e8f8ff6abfdb7084e2f778e79e822707c5
jainish4126 <pateljainish2021@gmail.com>
2025-08-13 23:08:17 +0530

    save final changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,15 +4,12 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer id="footer">
       <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
         <div className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+              QuickCart is your one-stop online shopping destination, designed to make buying and selling easy, fast, and secure. Discover a wide range of products, enjoy seamless checkout, and experience reliable delivery right to your doorstep. Our mission is to provide a convenient and enjoyable shopping experience for everyone. Thank you for choosing QuickCart!
           </p>
         </div>
 
@@ -24,10 +21,10 @@ const Footer = () => {
                 <a className="hover:underline transition" href="#">Home</a>
               </li>
               <li>
-                <a className="hover:underline transition" href="#">About us</a>
+                <a className="hover:underline transition" href="#footer">About us</a>
               </li>
               <li>
-                <a className="hover:underline transition" href="#">Contact us</a>
+                <a className="hover:underline transition" href="#footer">Contact us</a>
               </li>
               <li>
                 <a className="hover:underline transition" href="#">Privacy policy</a>
@@ -40,14 +37,14 @@ const Footer = () => {
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
-              <p>+1-234-567-890</p>
-              <p>contact@greatstack.dev</p>
+              <p>+91 6354024920</p>
+              <p>pateljainish2021@gmail.com</p>
             </div>
           </div>
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © GreatStack.dev All Right Reserved.
+        Copyright 2025 © QuickCart All Right Reserved.
       </p>
     </footer>
   );
--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -7,8 +7,7 @@ const NewsLetter = () => {
         Subscribe now & get 20% off
       </h1>
       <p className="md:text-base text-gray-500/80 pb-8">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
+          Newsletter subscription will be available soon. Stay tuned for updates and new features!
       </p>
       <div className="flex items-center justify-between max-w-2xl w-full md:h-14 h-12">
         <input
